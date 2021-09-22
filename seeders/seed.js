637099fc6da429b47ce17db971d2c415aad4e660
Lisette Colon <78636815+CodebyL@users.noreply.github.com>
2021-09-21 23:17:44 -0400

    Update seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true,
